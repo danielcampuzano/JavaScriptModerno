2301dbdccfcee0ad67c76315aa3496d2e6176dd6
Daniel Campuzano <daniel.campuzano@experimentality.co>
2022-03-07 20:48:59 -0500

    Se agrega codigo de clases en javascript

--- 01-fundamentos/assets/js/bases/alerts.js
+++
@@ -1,6 +0,0 @@
-//alert('Hola mundo');
-
-//let nombre = prompt('Cual es tu nombre?');
-
-const seleccion = confirm('Esta seguro de borrar esto?');
-console.log(seleccion);
--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,70 +1,174 @@
-/**
- * 2C - 2 of Clubs (Treboles)
- * 2D - 2 of Diamonds (Diamantes)
- * 2H - 2 of Hearts (Corazones)
- * 2E - 2 of Spades (Treboles)
- */
+const miModulo = (() => {
+    'use strict';
 
-let deck = [];
-const tipos = ['C', 'D', 'H', 'S'];
-const especiales = ['A', 'J', 'Q', 'K'];
+    let deck         = [];
+    const tipos      = ['C','D','H','S'],
+          especiales = ['A','J','Q','K'];
 
-//Esta funcion crea un nuevo deck
-const crearDeck = () => {
+    let puntosJugadores = [];
 
-    for (let i = 2; i <= 10; i++) {
+    // Referencias del HTML
+    const btnPedir   = document.querySelector('#btnPedir'),
+          btnDetener = document.querySelector('#btnDetener'),
+          btnNuevo   = document.querySelector('#btnNuevo');
 
-        for (let tipo of tipos) {
-            deck.push(i + tipo);
+    const divCartasJugadores = document.querySelectorAll('.divCartas'),
+          puntosHTML = document.querySelectorAll('small');
+
+
+    // Esta función inicializa el juego 
+    const inicializarJuego = ( numJugadores = 2 ) => {
+        deck = crearDeck();
+
+        puntosJugadores = [];
+        for( let i = 0; i< numJugadores; i++ ) {
+            puntosJugadores.push(0);
+        }
+        
+        puntosHTML.forEach( elem => elem.innerText = 0 );
+        divCartasJugadores.forEach( elem => elem.innerHTML = '' );
+
+        btnPedir.disabled   = false;
+        btnDetener.disabled = false;
+
+    }
+
+    // Esta función crea un nuevo deck
+    const crearDeck = () => {
+
+        deck = [];
+        for( let i = 2; i <= 10; i++ ) {
+            for( let tipo of tipos ) {
+                deck.push( i + tipo);
+            }
         }
 
+        for( let tipo of tipos ) {
+            for( let esp of especiales ) {
+                deck.push( esp + tipo);
+            }
+        }
+        return _.shuffle( deck );;
     }
 
-    for (let tipo of tipos) {
+    // Esta función me permite tomar una carta
+    const pedirCarta = () => {
+        if ( deck.length === 0 ) {
+            throw 'No hay cartas en el deck';
+        }
+        return deck.pop();
+    }
 
-        for (let especial of especiales) {
-            deck.push(especial + tipo);
+    const valorCarta = ( carta ) => {
+        const valor = carta.substring(0, carta.length - 1);
+        return ( isNaN( valor ) ) ? 
+                ( valor === 'A' ) ? 11 : 10
+                : valor * 1;
+    }
+
+    // Turno: 0 = primer jugador y el último será la computadora
+    const acumularPuntos = ( carta, turno ) => {
+        puntosJugadores[turno] = puntosJugadores[turno] + valorCarta( carta );
+        puntosHTML[turno].innerText = puntosJugadores[turno];
+        return puntosJugadores[turno];
+    }
+
+    const crearCarta = ( carta, turno ) => {
+
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+        imgCarta.classList.add('carta');
+        divCartasJugadores[turno].append( imgCarta );
+
+    }
+
+    const determinarGanador = () => {
+
+        const [ puntosMinimos, puntosComputadora ] = puntosJugadores;
+
+        setTimeout(() => {
+            if( puntosComputadora === puntosMinimos ) {
+                alert('Nadie gana :(');
+            } else if ( puntosMinimos > 21 ) {
+                alert('Computadora gana')
+            } else if( puntosComputadora > 21 ) {
+                alert('Jugador Gana');
+            } else {
+                alert('Computadora Gana')
+            }
+        }, 100 );
+
+    }
+
+    // turno de la computadora
+    const turnoComputadora = ( puntosMinimos ) => {
+
+        let puntosComputadora = 0;
+
+        do {
+            const carta = pedirCarta();
+            puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1 );
+            crearCarta(carta, puntosJugadores.length - 1 );
+
+        } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+        determinarGanador();
+    }
+
+
+
+    // Eventos
+    btnPedir.addEventListener('click', () => {
+
+        const carta = pedirCarta();
+        const puntosJugador = acumularPuntos( carta, 0 );
+        
+        crearCarta( carta, 0 );
+
+
+        if ( puntosJugador > 21 ) {
+            console.warn('Lo siento mucho, perdiste');
+            btnPedir.disabled   = true;
+            btnDetener.disabled = true;
+            turnoComputadora( puntosJugador );
+
+        } else if ( puntosJugador === 21 ) {
+            console.warn('21, genial!');
+            btnPedir.disabled   = true;
+            btnDetener.disabled = true;
+            turnoComputadora( puntosJugador );
         }
 
-    }
-
-    deck = _.shuffle(deck);
-
-    console.log(deck);
-
-    return deck;
-
-}
-
-crearDeck();
+    });
 
 
-//Esta funcion me permite tomar una carte
-const pedirCarta = () => {
+    btnDetener.addEventListener('click', () => {
+        btnPedir.disabled   = true;
+        btnDetener.disabled = true;
 
-    if (deck.length === 0) {
-        throw 'No hay cartas en el deck';
-    }
+        turnoComputadora( puntosJugadores[0] );
+    });
 
-    const carta = deck.pop();
+    // btnNuevo.addEventListener('click', () => {
+        
+    //     inicializarJuego();
 
-    console.log(deck);
-    console.log(carta);
+    // });
 
-    return carta;
-}
 
-//pedirCarta();
+    //Lo que retorne aca va a ser publico
+    //Pero solo accesible a traves de mi modulo -> Al inicio de la funcion hizo
+    //const miModulo = () = {} toda la funcion autoinvocada
+    return {
+        nuevoJuego: inicializarJuego
+    };
 
-const valorCarta = (carta) => {
+})();
 
-    const valor = carta.substr(0, carta.length - 1);
 
-    return (isNaN(valor)) ?
-        (valor === 'A') ? 11 : 10 :
-        valor * 1;
 
-}
 
-const valor = valorCarta(pedirCarta());
-console.log(valor);
+
+
+
+
