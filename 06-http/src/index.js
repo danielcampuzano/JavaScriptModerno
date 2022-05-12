4d2bfc90f865a51bbe197c5c034d184039b4fb6b
Daniel Campuzano <daniel.campuzano@experimentality.co>
2022-05-11 21:32:13 -0500

    Fin del curso y proyecto jose

---
+++ 06-http/src/index.js
@@ -0,0 +1,43 @@
+//import * as CRUD from './js/crud-provider'
+//import {init} from './js/chistes-page'
+//import { obtenerUsuarios } from './js/http-provider'
+//import {init} from './js/usuarios-page'
+//import { obtenerChiste } from './js/http-provider';
+//fetch( jokeUrl ).then( resp => {
+
+import { init } from './js/archivos-page';
+
+//    resp.json().then( ({id, value}) => {
+//        console.log(id);
+//        console.log(value);
+//    });
+
+//});
+
+//PROMESAS EN CADENA
+//fetch( jokeUrl )
+//    .then( resp => resp.json() )
+//    .then( jsonResponse => console.log(jsonResponse) );
+
+//obtenerChiste().then( jsonRespuesta => console.log(jsonRespuesta) );
+
+//obtenerUsuarios().then( usuarios => console.log(usuarios));
+
+//init();
+
+//CRUD.getUsuario( 2 ).then( usuario => console.log(usuario) );
+
+//CRUD.crearUsuario({
+//    name: 'Campu',
+//    job: 'Programmer'
+//}).then(usuario => console.log(usuario));
+
+//CRUD.actualizarUsuario( 1, {
+//     name: 'Melissa',
+//     job: 'Developer'
+//}).then( console.log );
+
+
+//CRUD.borrarUsuario( 1 ).then( console.log );
+
+init();
